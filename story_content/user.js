0329adedb257138d9dad877f84cd7c579187c285
friescaadl17 <friescaadelia17@gmail.com>
2024-12-21 11:16:05 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6U49eO7jBN1":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="1.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=0.4;
+}
+
